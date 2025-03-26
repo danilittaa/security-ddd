39e28444c59a901a1b21f9172544ab2242784a93
Dana Todoriak <todoriak@gmail.com>
2025-03-26 18:09:48 +0200

    initial commit

---
+++ src/main/java/com/todoriak/securityddd/SecurityDddApplication.java
@@ -0,0 +1,13 @@
+package com.todoriak.securityddd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityDddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityDddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/todoriak/securityddd/SecurityDddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todoriak.securityddd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityDddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
