ef621c4ae1d1bba76609414d7952b8b1d88e8b79
Dana Todoriak <todoriak@gmail.com>
2025-04-15 17:06:50 +0300

    lab5 - add more users and access restriction

--- src/main/java/com/todoriak/securityddd/config/SecurityConfig.java
+++ src/main/java/com/todoriak/securityddd/config/SecurityConfig.java
@@ -14,6 +14,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -21,11 +27,45 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
+    public static PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .authorizeHttpRequests(req ->
+                        req.requestMatchers("/index.html").permitAll()
+                                .requestMatchers("/api/v1/catHotels/hello-admin").hasRole("ADMIN")
+                                .requestMatchers("/api/v1/catHotels/hello-user").hasRole("USER")
+                                .requestMatchers("/api/v1/catHotels/hello-unknown").hasAnyRole("USER", "ADMIN", "SUPERADMIN")
+                                .requestMatchers("/api/v1/catHotels/**").hasRole("SUPERADMIN")
+                                .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults());
 
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles("ADMIN")
+                .build();
+
+        UserDetails user = User.builder()
+                .username("user")
+                .password(passwordEncoder().encode("user"))
+                .roles("USER")
+                .build();
+
+        UserDetails superAdmin = User.builder()
+                .username("superAdmin")
+                .password(passwordEncoder().encode("superAdmin"))
+                .roles("SUPERADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, user, superAdmin);
     }
 }
